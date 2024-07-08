6be6234b6755f1015b88354637f1eced7e695253
Winston <winston87245@gmail.com>
2024-07-09 07:36:35 +0800

    dist

---
+++ dist/components/Input/index.js
@@ -0,0 +1,11 @@
+import { jsx as o } from "react/jsx-runtime";
+import '../../assets/index2.css';const u = "_input_1auw0_1", p = {
+  input: u
+};
+function c(t) {
+  const { className: n, ...s } = t;
+  return /* @__PURE__ */ o("input", { className: `${n} ${p.button}`, ...s });
+}
+export {
+  c as Input
+};
---
+++ dist/components/Label/index.js
@@ -0,0 +1,11 @@
+import { jsx as t } from "react/jsx-runtime";
+import '../../assets/index.css';const o = "_label_mog69_1", a = {
+  label: o
+};
+function r(l) {
+  const { className: s, ...e } = l;
+  return /* @__PURE__ */ t("label", { className: `${s} ${a.button}`, ...e });
+}
+export {
+  r as Label
+};
