e975206e7be7e75747fb3e6f7298d47ac05bcd18
Winston <winston87245@gmail.com>
2024-07-09 11:09:05 +0800

    example

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import './App.css';
 import { SipContextProvider, SipContext, PhoneComponent } from '../lib/store/SipContextProvider'; // Correct import path
 
 function App() {
-  const userAgentOptions = {
-    authorizationUsername: "user1",
-    authorizationPassword: "password",
-  }
   const sipConfig = {
     baseUri: "sip.example.com",
     server: "ws:sip.example.com",
